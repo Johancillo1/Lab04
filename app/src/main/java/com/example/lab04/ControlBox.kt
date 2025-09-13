01eae7e78688a1108c9328255d543ca225ed75b0
Johan Vallejos <johancillo123@gmail.com>
2025-09-12 20:06:08 -0500

    feat: add ControlBox composable

--- app/src/main/java/com/example/lab04/ControlBox.kt
+++ app/src/main/java/com/example/lab04/ControlBox.kt
@@ -1,4 +1,28 @@
 package com.example.lab04
 
-class ControlBox {
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ControlBox() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(onClick = { }) {
+            Text("Soy un botón dentro de un Box")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewControlBox() {
+    ControlBox()
 }
